d2658d1238ba504737a497f37c2fbe64b628c220
Diakaridia KANTE <kantediakante@gmail.com>
2023-01-10 09:05:04 +0000

    fc

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Forum',
+      title: 'Flutter Forum 1.0',
       home: LoginPage(),
     );
   }
